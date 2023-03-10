f874e155cfbc6be574daea8ffdced977c5bae730
Naman Saini <pinuchinu34@gmail.com>
2023-03-10 13:57:10 +0530

    initial commit

--- credentials/secure.js
+++ credentials/secure.js
@@ -1,5 +1,5 @@
 export var octokitAuthKey = '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';
-export var repoOwner = 'naman77s';
+export var repoOwner = 'realpxd';
 export var repoName = 'quizapi';
 export var smtpAuthKey = "9674fe91-144f-470d-8d83-ca3e9f68fd19";
 export var smtpEmail = "pinuchinu34@gmail.com";
