53fb881a2964566cf1b03a266f2e0596c21efacc
ProgrammerXD <thisisnamansaini@gmail.com>
2024-01-04 20:38:37 +0530

    Update secure.js

--- credentials/secure.js
+++ credentials/secure.js
@@ -1,8 +1,8 @@
-export var octokitAuthKey = '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';
+export var octokitAuthKey = 'hidden';
 export var repoOwner = 'realpxd';
 export var repoName = 'quizapi';
-export var smtpAuthKey = "9674fe91-144f-470d-8d83-ca3e9f68fd19";
-export var smtpEmail = "pinuchinu34@gmail.com";
+export var smtpAuthKey = "hidden";
+export var smtpEmail = "hidden";
 export var databaseName = "userData.json";
 export var commiterName = "Naman Saini";
 export var commiterEmail = "thisispxd@gmail.com";
